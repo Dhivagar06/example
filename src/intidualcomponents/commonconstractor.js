b3d5d43ac6b2c31ced060d9f2ff7c3e54b6eb505
Dhivagar22 <142041021+Dhivagar22@users.noreply.github.com>
2023-10-05 18:22:31 +0530

    try

--- src/intidualcomponents/commonconstractor.js
+++ src/intidualcomponents/commonconstractor.js
@@ -1,18 +1,33 @@
-import React from "react";
-import Intidualdate from "../inticer/been";
+import React, { useState } from "react";
 import { Qwe } from "../inticer/beendate";
 
+function Commonstructor(props) {
+  const [message, setMessage] = useState(null);
 
-function Commonstructor(props){
-    return(
-        <div>
-            <h1>{props.heading}</h1>
-            <img src={props.url} alt="image" onClick={(id)=>(
-              Intidualdate.id=== Qwe.id? Qwe.id:<h1>Not bound</h1>
-            )}></img>
-        </div>
-    )
+  const handleImageClick = () => {
+    const clickedId = props.id; // Get the id from the props
+
+    // Check if the clicked image's id matches any id in Qwe
+    const match = Qwe.find((item) => item.id === clickedId);
+
+    if (match) {
+      setMessage(`Clicked image with id: ${clickedId}`);
+    } else {
+      setMessage("Not found");
+    }
+  }
+
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <img
+        src={props.url}
+        alt="image"
+        onClick={handleImageClick}
+      />
+      {message && <p>{message}</p>}
+    </div>
+  );
 }
-
 
 export default Commonstructor;
